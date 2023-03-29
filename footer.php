bdb958a4b31902319603d53b6f02d223e79ec0e2
Matthew-Riddell <d00245674@student.dkit.ie>
2023-03-29 14:47:55 +0100

    CreateQuestions, CSS Bootstrap, Home Page, Header and Footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="footer fixed-bottom bg-dark text-light py-3">
+  <div class="container text-center">
+    <p class="mb-0">&copy; <?php echo date("Y"); ?> DkIT Hackathon - EnQUIZment Team - 2023</p>
+  </div>
+</footer>
---
+++ nav.php
@@ -0,0 +1,31 @@
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Nav Bar">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="index.php">
+      <img src="images/nasa.png" alt="Logo" width="40" height="33" class="d-inline-block align-text-top me-2">
+      EnQUIZment
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="CreateQuestions.php">Create Questions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="sign-up.php">Sign Up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="contact-form.php">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+    
