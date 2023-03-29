107111b2ecd7af840b78209722066f10404b8c2a
Matthew-Riddell <d00245674@student.dkit.ie>
2023-03-29 16:03:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+require_once('database.php');
 
+// Get Missions
+$queryMissions = 'SELECT * FROM missions';
+$statement = $db->prepare($queryMissions);
+$statement->execute();
+$missions = $statement->fetchAll();
+$statement->closeCursor();
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,8 +30,21 @@
       <h3>EnQUIZment</h3>
     </div>
     <div class="container">
+  <div class="row">
+    <?php foreach ($missions as $mission) : ?>
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <img class="card-img-top" src="<?php echo $mission['image']; ?>" alt="<?php echo $mission['mission_name']; ?>" height = "250px" width = "200px">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo $mission['mission_name']; ?></h5>
+            <a href="mission.php?id=<?php echo $mission['mission_id']; ?>" class="btn btn-primary">More Details</a>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
 
-    </div>
   </div>
 
     </section><br><br><br>
