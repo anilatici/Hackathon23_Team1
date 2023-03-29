78ae3e22a96e902a20a0c2b11b155065e40f13ed
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-29 22:59:57 +0800

    update for login add question

--- addQuestion.php
+++ addQuestion.php
@@ -5,42 +5,66 @@ error_reporting(E_ALL);
 include'database.php';
 $title = $_POST['title'];
 $questions = $_POST['questions'];
-foreach($questions as $question){
-    var_dump($question);
-    echo '<br>';
-    $sql = "INSERT INTO questions (question_text) VALUES (?)";
+    $sql = "INSERT INTO questionGroups (title) VALUES (?)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(1, $title);
     $stmt->execute();
     $stmt->closeCursor();
     $lastId = $conn->lastInsertId();
-    if($lastId == 0){
-        echo "error";
-    } else {
-        if ($question["type"] == 1) {
-            if ($lastId == 0) {
-                echo "error";
-            } else {
-                $sql = "INSERT INTO answers (question_id, answer_text,is_correct) VALUES (?,?,?)";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $lastId);
-                $stmt->bindParam(2, $question["answer"]);
-                $str = 1;
-                $stmt->bindParam(3, $str);
-                $stmt->execute();
-                $stmt->closeCursor();
-            }
-        } elseif ($question["type"] == 2) {
-            if ($lastId == 0) {
-                echo "error";
-            } else {
+
+    foreach($questions as $question) {
+        $sql = "INSERT INTO questions (question_text,question_group) VALUES (?,?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(1, $question["question"]);
+        $stmt->bindParam(2, $lastId);
+        $stmt->execute();
+        $stmt->closeCursor();
+        $lastId = $conn->lastInsertId();
+        if ($lastId == 0) {
+            echo "error";
+        } else {
+            if ($question["type"] == 1) {
+                if ($lastId == 0) {
+                    echo "error";
+                } else {
+                    $sql = "INSERT INTO answers (question_id, answer_text,is_correct) VALUES (?,?,?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bindParam(1, $lastId);
+                    $stmt->bindParam(2, $question["answer"]);
+                    $str = 1;
+                    $stmt->bindParam(3, $str);
+                    $stmt->execute();
+                    $stmt->closeCursor();
+                }
+            } elseif ($question["type"] == 2) {
+                if ($lastId == 0) {
+                    echo "error";
+                } else {
+                    $count = 0;
+                    foreach ($question["answer"] as $answer) {
+                        $sql = "INSERT INTO answers (question_id, answer_text,is_correct) VALUES (?,?,?)";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bindParam(1, $lastId);
+                        $stmt->bindParam(2, $answer["answer"]);
+                        if ($count == $question["correctAns"]) {
+                            $str = 1;
+                        } else {
+                            $str = 0;
+                        }
+                        $stmt->bindParam(3, $str);
+                        $stmt->execute();
+                        $stmt->closeCursor();
+                        $count++;
+                    }
+                }
+            } elseif ($question["type"] == 3) {
                 $count = 0;
                 foreach ($question["answer"] as $answer) {
                     $sql = "INSERT INTO answers (question_id, answer_text,is_correct) VALUES (?,?,?)";
                     $stmt = $conn->prepare($sql);
                     $stmt->bindParam(1, $lastId);
-                    $stmt->bindParam(2, $answer["answer"]);
-                    if ($count == $question["correctAns"]) {
+                    $stmt->bindParam(2, $answer);
+                    if (isset($question["correctAns"][$count])) {
                         $str = 1;
                     } else {
                         $str = 0;
@@ -51,28 +75,9 @@ foreach($questions as $question){
                     $count++;
                 }
             }
-        } elseif ($question["type"] == 3) {
-            $count = 0;
-            foreach ($question["answer"] as $answer) {
-                $sql = "INSERT INTO answers (question_id, answer_text,is_correct) VALUES (?,?,?)";
-                $stmt = $conn->prepare($sql);
-                $stmt->bindParam(1, $lastId);
-                $stmt->bindParam(2, $answer);
-                if (isset($question["correctAns"][$count])) {
-                    $str = 1;
-                } else {
-                    $str = 0;
-                }
-                $stmt->bindParam(3, $str);
-                $stmt->execute();
-                $stmt->closeCursor();
-                $count++;
-            }
         }
     }
 
     header("Location: index.php");
-
-}
 
 ?>
