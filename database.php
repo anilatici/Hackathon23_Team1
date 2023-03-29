58553b956f7423ddc39eb09a637ced9da1a62f07
Lee Xuan Ong <ongtan597@gmail.com>
2023-03-29 21:16:22 +0800

    db connection work in progress waiting for abiel

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$dsn = 'mysql:host=localhost;dbname=';
+$username = 'root';
+$password = '';
+
+// TODO: notes to remember
+// 1. The sql query that was used on the server is CASE SENSITIVE.
+try {
+    $conn = new PDO($dsn, $username, $password);
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include('database_error.php');
+    exit();
+}
